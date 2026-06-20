f454ab2d1bb3fa2bc69bb735f342e10c4dfec9a7
Lorena Ferreira <lollaaz@icloud.com>
2026-06-20 18:17:02 +0200

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Mari Rodrigues Fotografia</h1>
+      <p>Site em construção</p>
+    </main>
+  );
+}
+
+export default App;
